a2fed1c8ca2f61521ffc366bb540f56d5775e080
johnrukstalis <theveterandev@gmail.com>
2025-11-23 10:36:52 -0500

    inital commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"encoding/json"
+	"log/slog"
+	"net/http"
+)
+
+func HttpError(w http.ResponseWriter, msg string, err error, code int) {
+	slog.Error(msg, "error", err)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+
+	json.NewEncoder(w).Encode(map[string]string{
+		"error": msg,
+	})
+}
